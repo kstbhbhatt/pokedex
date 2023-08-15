7aef6a4386b1d375987eb9622ae49856288d973d
kstbhbhatt <kstbhbhatt@gmail.com>
2023-08-15 15:24:07 +0530

    mobile changes

--- src/Components/BackButton/BackButton.jsx
+++ src/Components/BackButton/BackButton.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import "./backButton.css";
 
 function BackButton({ onClick }) {
-  return <div className="back-button" onClick={() => onClick()}></div>;
+  return (
+    <div className="back-button" onClick={() => onClick()}>
+      <div className="back-btn"></div>
+    </div>
+  );
 }
 export default BackButton;
